d3bb71c9942b1001e375f08655a43a816870462f
Retouchable <109464156+Retouchable@users.noreply.github.com>
2024-03-06 20:04:51 +0700

    add google auth

--- app/src/main/java/www/smktelkommalang/sch/id/rental_room/Model/Login/LoginActivity.kt
+++ app/src/main/java/www/smktelkommalang/sch/id/rental_room/Model/Login/LoginActivity.kt
@@ -1,67 +1,87 @@
 package www.smktelkommalang.sch.id.rental_room.Model.Login
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import www.smktelkommalang.sch.id.rental_room.Fragment.Fragment.HomeFragment
 import www.smktelkommalang.sch.id.rental_room.MainActivity
 import www.smktelkommalang.sch.id.rental_room.Model.Register.RegisterActivity
 import www.smktelkommalang.sch.id.rental_room.Model.ResetPassword.ResetPasswordActivity
+import www.smktelkommalang.sch.id.rental_room.R
 import www.smktelkommalang.sch.id.rental_room.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var binding : ActivityLoginBinding
-    lateinit var auth : FirebaseAuth
-
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        
         auth = FirebaseAuth.getInstance()
-
+        
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        
         binding.forgotPassword.setOnClickListener {
             val intent = Intent(this, ResetPasswordActivity::class.java)
             startActivity(intent)
         }
-
+        
         binding.tvToRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
+        
         binding.btnLogin.setOnClickListener {
             val email = binding.edtEmailLogin.text.toString()
             val password = binding.edtPasswordLogin.text.toString()
-
+            
             //Validasi email
-            if (email.isEmpty()){
+            if (email.isEmpty()) {
                 binding.edtEmailLogin.error = "Email Harus Diisi"
                 binding.edtEmailLogin.requestFocus()
                 return@setOnClickListener
             }
-
+            
             //Validasi email tidak sesuai
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 binding.edtPasswordLogin.error = "Email Tidak Valid"
                 binding.edtPasswordLogin.requestFocus()
                 return@setOnClickListener
             }
-
+            
             //Validasi password
-            if (password.isEmpty()){
+            if (password.isEmpty()) {
                 binding.edtPasswordLogin.error = "Password Harus Diisi"
                 binding.edtPasswordLogin.requestFocus()
                 return@setOnClickListener
             }
-
-            LoginFirebase(email,password)
+            
+            
+            loginFirebase(email, password)
+        }
+        
+        binding.btnGoogleLogin.setOnClickListener {
+            googleSignInClient = GoogleSignIn.getClient(this, gso)
+            loginGoogle()
         }
     }
-
-    private fun LoginFirebase(email: String, password: String) {
+    
+    private fun loginFirebase(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) {
                 if (it.isSuccessful) {
@@ -72,5 +92,46 @@ class LoginActivity : AppCompatActivity() {
                     Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+    
+    private fun loginGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+    
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+                print("result ok")
+            }
+        }
+    
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            } else {
+                Toast.makeText(this, "account is null", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+    
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        print(credential)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Selamat datang ${account.email}", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
