af71abb9a0d395605697b273201601d32653b65a
Retouchable <109464156+Retouchable@users.noreply.github.com>
2024-03-15 22:37:40 +0700

    fix: opening

--- app/src/main/java/www/smktelkommalang/sch/id/rental_room/Fragment/FragmentOpening/OpeningActivity.kt
+++ app/src/main/java/www/smktelkommalang/sch/id/rental_room/Fragment/FragmentOpening/OpeningActivity.kt
@@ -1,34 +1,25 @@
 package www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import www.smktelkommalang.sch.id.rental_room.Adapter.ViewPagerAdapter
 import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.FragmentOpeningPage.OpeningPage1Fragment
 import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.FragmentOpeningPage.OpeningPage2Fragment
 import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.FragmentOpeningPage.OpeningPage3Fragment
-import www.smktelkommalang.sch.id.rental_room.databinding.FragmentOpeningBinding
 
-class OpeningFragment : Fragment() {
-    
-    private lateinit var binding: FragmentOpeningBinding
-    
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentOpeningBinding.inflate(layoutInflater)
+class OpeningActivity : AppCompatActivity() {
+    lateinit var binding: OpeningActivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = OpeningActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupTab()
-        return binding.root
     }
     
     private fun setupTab() {
         val dotsIndicator = binding.dotsIndicator
         val viewPager = binding.viewPager
-        val adapter = ViewPagerAdapter(childFragmentManager)
+        val adapter = ViewPagerAdapter(supportFragmentManager)
         adapter.addFragment(OpeningPage1Fragment(),"Dalam Proses")
         adapter.addFragment(OpeningPage2Fragment(),"Dalam Proses")
         adapter.addFragment(OpeningPage3Fragment(),"Dalam Proses")
@@ -36,4 +27,6 @@ class OpeningFragment : Fragment() {
         viewPager.adapter = adapter
         dotsIndicator.setViewPager(viewPager)
     }
+    }
+    
 }
