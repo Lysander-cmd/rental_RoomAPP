ed65b86190cc1edf9a72d38dc604beba638e428e
Retouchable <109464156+Retouchable@users.noreply.github.com>
2024-03-16 22:00:26 +0700

    fix: opening after splash

--- app/src/main/java/www/smktelkommalang/sch/id/rental_room/SplashScreen/SplashScreenActivity.kt
+++ app/src/main/java/www/smktelkommalang/sch/id/rental_room/SplashScreen/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import www.smktelkommalang.sch.id.rental_room.ViewModel.Authenticate.Opening.OpeningActivity
 import www.smktelkommalang.sch.id.rental_room.ViewModel.Authenticate.Login.LoginActivity
 import www.smktelkommalang.sch.id.rental_room.R
 
@@ -17,7 +18,7 @@ class SplashScreenActivity : AppCompatActivity() {
         val ivNote = findViewById<ImageView>(R.id.iv_note)
         ivNote.alpha = 0f
         ivNote.animate().setDuration(1500).alpha(1f).withEndAction {
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, OpeningActivity::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
---
+++ app/src/main/java/www/smktelkommalang/sch/id/rental_room/ViewModel/Authenticate/Opening/OpeningActivity.kt
@@ -0,0 +1,66 @@
+package www.smktelkommalang.sch.id.rental_room.ViewModel.Authenticate.Opening
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import www.smktelkommalang.sch.id.rental_room.Adapter.ViewPagerAdapter
+import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.OpeningPage1Fragment
+import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.OpeningPage2Fragment
+import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.OpeningPage3Fragment
+import www.smktelkommalang.sch.id.rental_room.MainActivity
+import www.smktelkommalang.sch.id.rental_room.ViewModel.Authenticate.Login.LoginActivity
+import www.smktelkommalang.sch.id.rental_room.databinding.ActivityLoginBinding
+import www.smktelkommalang.sch.id.rental_room.databinding.ActivityMainBinding
+import www.smktelkommalang.sch.id.rental_room.databinding.ActivityOpeningBinding
+
+class OpeningActivity : AppCompatActivity() {
+    
+    lateinit var binding: ActivityOpeningBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOpeningBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupTab()
+    }
+    
+    private fun setupTab() {
+        var currentPage = 0
+        val btnSkip = binding.btnSkip
+        val btnNext = binding.btnNext
+        val viewPager = binding.viewPager
+        val dotsIndicator = binding.dotsIndicator
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        
+        adapter.addFragment(OpeningPage1Fragment(), "Dalam Proses")
+        adapter.addFragment(OpeningPage2Fragment(), "Dalam Proses")
+        adapter.addFragment(OpeningPage3Fragment(), "Dalam Proses")
+        viewPager.adapter = adapter
+        dotsIndicator.setViewPager(viewPager)
+        
+        btnNext.setOnClickListener {
+            if (currentPage == 2) {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            } else {
+                currentPage++
+                viewPager.currentItem = currentPage
+                if (currentPage == 2) {
+                    btnNext.text = "Masuk"
+                    btnSkip.visibility = View.INVISIBLE
+                } else {
+                    btnNext.text = "Selanjutnya"
+                }
+            }
+        }
+        
+        btnSkip.setOnClickListener {
+            currentPage = 2
+            viewPager.currentItem = 2
+            btnSkip.visibility = View.INVISIBLE
+            btnNext.text = "Masuk"
+        }
+    }
+}
+
