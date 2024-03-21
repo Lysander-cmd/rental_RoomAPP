12a3ae3fc46989d620fb47b44df8f9180b33c259
Retouchable <109464156+Retouchable@users.noreply.github.com>
2024-03-21 21:42:54 +0700

    fix: UI profile

--- app/src/main/java/www/smktelkommalang/sch/id/rental_room/Database/RuanganDatabase.kt
+++ app/src/main/java/www/smktelkommalang/sch/id/rental_room/Database/RuanganDatabase.kt
@@ -48,4 +48,41 @@ class RuanganDatabase {
             }
         })
     }
+    
+    fun getRuanganDetailData(
+        resp: (ArrayList<RuanganData>) -> Unit,
+    ) {
+        db = FirebaseDatabase.getInstance().reference
+        val ruanganRef = db.child("ruangan")
+        ruanganRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val ruanganDataList = ArrayList<RuanganData>()
+                if (snapshot.exists()) {
+                    for (dataSnapshot in snapshot.children) {
+                        val title: String =
+                            dataSnapshot.child("title").getValue(String::class.java).toString()
+                        val ukuran: String =
+                            dataSnapshot.child("ukuran").getValue(String::class.java).toString()
+                        val kapasitas: String =
+                            dataSnapshot.child("kapasitas").getValue(String::class.java).toString()
+                        val image: String =
+                            dataSnapshot.child("image").getValue(String::class.java).toString()
+                        val ruanganData =
+                            RuanganData(
+                                image = image,
+                                ukuran = ukuran,
+                                kapasitas = kapasitas,
+                                title = title
+                            )
+                        ruanganDataList.add(ruanganData)
+                    }
+                }
+                resp(ruanganDataList)
+            }
+            
+            override fun onCancelled(error: DatabaseError) {
+                println("Error: ${error.message}")
+            }
+        })
+    }
 }
