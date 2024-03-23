46b78ec0185106a0b656d5116cee112a4d0eaa2f
Retouchable <109464156+Retouchable@users.noreply.github.com>
2024-03-23 12:49:44 +0700

    fix ui

--- app/src/main/java/www/smktelkommalang/sch/id/rental_room/Fragment/FragmentBeranda/BerandaFragment.kt
+++ app/src/main/java/www/smktelkommalang/sch/id/rental_room/Fragment/FragmentBeranda/BerandaFragment.kt
@@ -5,17 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
+import www.smktelkommalang.sch.id.rental_room.Adapter.RecyclerViewRuanganAdapter
+import www.smktelkommalang.sch.id.rental_room.Adapter.ViewPagerAdapter
+import www.smktelkommalang.sch.id.rental_room.Database.RuanganDatabase
+import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.OpeningPage1Fragment
+import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.OpeningPage2Fragment
+import www.smktelkommalang.sch.id.rental_room.Fragment.FragmentOpening.OpeningPage3Fragment
+import www.smktelkommalang.sch.id.rental_room.Model.Ruangan.RuanganData
 import www.smktelkommalang.sch.id.rental_room.R
 
-
 class BerandaFragment : Fragment() {
-
+    private lateinit var ruanganDatabase: RuanganDatabase
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var ruanganDataList: ArrayList<RuanganData>
+    private lateinit var recyclerViewRuanganAdapter: RecyclerViewRuanganAdapter
+    private lateinit var viewPager: ViewPager
+    private lateinit var dotsIndicator: DotsIndicator
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        return view
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        viewPager = rootView.findViewById(R.id.viewPager)
+        dotsIndicator = rootView.findViewById(R.id.dotsIndicator)
+        recyclerView = rootView.findViewById(R.id.recycler_view)
+        
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
+        
+        ruanganDataList = ArrayList()
+        ruanganDatabase = RuanganDatabase()
+        recyclerViewRuanganAdapter = RecyclerViewRuanganAdapter(ruanganDataList)
+        recyclerView.adapter = recyclerViewRuanganAdapter
+        
+        readDataFromFirebase()
+        setupTab()
+        return rootView
+    }
+    
+    private fun readDataFromFirebase() {
+        ruanganDatabase.getRuanganData { dataList ->
+            ruanganDataList.clear()
+            ruanganDataList.addAll(dataList)
+            recyclerViewRuanganAdapter.notifyDataSetChanged()
+        }
+    }
+    
+    private fun setupTab() {
+        val adapter = ViewPagerAdapter(childFragmentManager)
+        
+        adapter.addFragment(OpeningPage1Fragment(), "1st")
+        adapter.addFragment(OpeningPage2Fragment(), "2nd")
+        adapter.addFragment(OpeningPage3Fragment(), "3rd")
+        
+        viewPager.adapter = adapter
+        dotsIndicator.setViewPager(viewPager)
     }
 }
